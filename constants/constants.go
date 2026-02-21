707ec6613db58e6052eefecd9bb4b215aba5ab39
Aaron Park <apark6507@gmail.com>
2026-02-21 15:59:47 -0500

    push yaml change

--- constants/constants.go
+++ constants/constants.go
@@ -8,8 +8,11 @@ const (
 	REQUEST_URL       = "REQUEST_URL"
 	GOPHER_RUNNER     = "gopher-runner"
 	POST_METHOD       = "POST"
+	PUT_METHOD        = "PUT"
 	GENERIC_ERROR     = "Generic Error"
 	STATUS_UPDATE_URL = "STATUS_UPDATE_URL"
 	SUCCESS           = "success"
 	FAILED            = "failed"
+	CLAIMED           = "Claimed"
+	FINISHED          = "Finished"
 )
